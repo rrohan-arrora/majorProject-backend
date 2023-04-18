9af1201e93524b9682d522aedf1c7481591d2efc
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-18 15:51:07 +0530

    Added find by title and find by author functionality in JPA repository of spring data JPA

--- src/main/java/com/iiitb/readandreturn/DAO/BookRepository.java
+++ src/main/java/com/iiitb/readandreturn/DAO/BookRepository.java
@@ -1,11 +1,17 @@
 package com.iiitb.readandreturn.DAO;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.iiitb.readandreturn.entity.Book;
 
 @CrossOrigin("http://localhost:3000")
 public interface BookRepository extends JpaRepository<Book, Long>{
-
+	
+	// pagebale helps in adding page and size to the url.
+	Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pagable);
+	Page<Book> findByAuthorContaining(@RequestParam("author") String author, Pageable pagable);
 }
