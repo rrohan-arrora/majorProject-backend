f77db02fd8ceb4547ca70d79205a440134f11ad4
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-18 12:20:43 +0530

    Added code over the BookRepository in order to remove CORS error

--- src/main/java/com/iiitb/readandreturn/DAO/BookRepository.java
+++ src/main/java/com/iiitb/readandreturn/DAO/BookRepository.java
@@ -1,9 +1,11 @@
 package com.iiitb.readandreturn.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.iiitb.readandreturn.entity.Book;
 
+@CrossOrigin("http://localhost:3000")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
