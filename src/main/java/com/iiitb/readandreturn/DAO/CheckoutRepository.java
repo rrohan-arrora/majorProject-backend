aa9370f78c2ee3304a13958cf6a893282510fd07
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-20 23:59:04 +0530

    Added method to check if the book is checkout by the user Added method to check number of book checkout by the user

--- src/main/java/com/iiitb/readandreturn/DAO/CheckoutRepository.java
+++ src/main/java/com/iiitb/readandreturn/DAO/CheckoutRepository.java
@@ -1,8 +1,13 @@
 package com.iiitb.readandreturn.DAO;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.iiitb.readandreturn.entity.Checkout;
 
+
+
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
 	Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+	List<Checkout> numberOfBookCheckoutByUser(String userEmail);
 }
--- src/main/java/com/iiitb/readandreturn/controller/BookController.java
+++ src/main/java/com/iiitb/readandreturn/controller/BookController.java
@@ -2,6 +2,7 @@ package com.iiitb.readandreturn.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,8 +25,14 @@ public class BookController {
 	
 	@PutMapping("/secure/checkout")
 	public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-		String userEmail = "testuser1@gmail.com";
+		String userEmail = "testuser@gmail.com";
 		
 		return bookService.checkoutBook(userEmail, bookId);
 	}
+	
+	@GetMapping("/secure/ischeckedout/byuser")
+    public Boolean checkoutBookByUser(@RequestParam Long bookId) {
+		String userEmail = "testuser@gmail.com";
+        return bookService.checkoutBookByUser(userEmail, bookId);
+    }
 }
