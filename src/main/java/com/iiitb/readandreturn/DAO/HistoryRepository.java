edb9a124c0d7528244878bd988ec96a1b9534244
Rohan Arora <an14092000@gmail.com>
2023-04-25 01:36:46 +0530

    Added history repository

---
+++ src/main/java/com/iiitb/readandreturn/DAO/HistoryRepository.java
@@ -0,0 +1,12 @@
+package com.iiitb.readandreturn.DAO;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.iiitb.readandreturn.entity.History;
+
+public interface HistoryRepository extends JpaRepository<History, Long>{
+	Page<History> findBooksByUserEmail(@RequestParam("email") String userEmail, Pageable pageable);
+}
